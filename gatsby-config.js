49ec0b33b30106a0fbb24c720edb53cd9e129f68
julian <ornatus30@gmail.com>
2020-05-01 03:30:27 -0400

    keyadd

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,10 +39,8 @@ module.exports = {
         collection: [`coffeItem`, `coffeProduct`],
         server: { address: `ds331548.mlab.com`, port: 31548 },
         auth: {
-          /* user: process.env.MONGOUSER,
-          password: process.env.MONGOPASSWORD, */
-          user: `julian`,
-          password: `Julian12345`,
+          user: process.env.MONGOUSER,
+          password: process.env.MONGOPASSWORD,
         },
       },
     },
